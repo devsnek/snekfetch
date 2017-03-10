a0bf85f7167247a11a57353868cae0bcdab14624
Gus Caplan <me@gus.host>
2017-03-10 01:29:05 -0600

    (╯°□°）╯︵ ┻━┻

--- src/mime.js
+++ src/mime.js
@@ -2,13 +2,13 @@ const mimes = require('./mimes');
 const mimeOfBuffer = require('./mimeOfBuffer');
 
 function lookupMime(ext) {
-  return mimes[ext] || mimes.eot;
+  return mimes[ext] || mimes.bin;
 }
 
 function lookupBuffer(buffer) {
   const type = mimeOfBuffer(buffer);
   if (type) return type.mime;
-  else return mimes.eot;
+  else return mimes.bin;
 }
 
 module.exports = {
