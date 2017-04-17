2c82db90707788a5e234ec4ec15cf7fbffd38ff2
snek <me@gus.host>
2017-04-17 00:45:34 -0500

    pass through end

--- src/index.js
+++ src/index.js
@@ -148,7 +148,7 @@ class Snekfetch extends Stream.Readable {
   }
 
   end(cb) {
-    this.then(
+    return this.then(
       (res) => cb ? cb(null, res) : res,
       (err) => cb ? cb(err, err.status ? err : null) : err
     );
