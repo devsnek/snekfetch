0ce04a166d0f8b75dd1605f8f2259e574882e45c
Gus Caplan <me@gus.host>
2017-10-27 22:34:44 -0500

    module, sonar

---
+++ esm.mjs
@@ -0,0 +1,12 @@
+import Snekfetch from '.';
+
+export default Snekfetch;
+
+export const get = Snekfetch.get;
+export const head = Snekfetch.head;
+export const post = Snekfetch.post;
+export const put = Snekfetch.put;
+export const delete = Snekfetch.delete;
+export const connect = Snekfetch.connect;
+export const options = Snekfetch.options;
+export const patch = Snekfetch.patch;
